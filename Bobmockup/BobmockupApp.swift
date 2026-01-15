d1ab1f743b4aa549b4d9d57d13de30c3048f31b3
BudgetApp <bob.oulhen@gmail.com>
2026-01-15 17:36:53 +0100

    Initial Commit

---
+++ Bobmockup/BobmockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobmockupApp.swift
+//  Bobmockup
+//
+//  Created by Robert Oulhen on 15/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BobmockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bobmockup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bobmockup
+//
+//  Created by Robert Oulhen on 15/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
